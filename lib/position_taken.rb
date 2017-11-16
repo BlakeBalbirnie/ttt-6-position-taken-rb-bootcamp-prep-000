bec99bad15aa61c33dfb1f5a6a6714f389b5085a
Blake Balbirnie <blakebalbirnie@gmail.com>
2017-11-16 20:23:53 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
-# code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "0"
+    true
+  else board[index] == " " || "" || nil
+    return false
+  end
+end
